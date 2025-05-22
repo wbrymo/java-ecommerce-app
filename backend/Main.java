8404a8d009c840b5a7d41002849f5297937fa875
wbrymo <111048430+wbrymo@users.noreply.github.com>
2025-05-22 19:02:18 +0100

    Update Main.java

--- backend/Main.java
+++ backend/Main.java
@@ -1,8 +1,11 @@
-
 package com.cyat.ecommerce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("E-commerce Backend Running...");
+        SpringApplication.run(Main.class, args);
     }
 }
